5c9b7e971a1a317ee8b2402fe5cf17d004ec9e15
Mika <test@Tests-MacBook-Pro.local>
2024-09-30 14:52:44 -0300

    refactor: change project utils url base

--- src/lib/utils/project-util.ts
+++ src/lib/utils/project-util.ts
@@ -1,7 +1,7 @@
 import { Project } from "@/src/app/home/types";
 
 export const fetchProjects = async (): Promise<Project[]> => {
-  const response = await fetch("http://localhost:3000/project");
+  const response = await fetch("http://localhost:3000/projects");
   const projects = await response.json();
   return projects;
 };
