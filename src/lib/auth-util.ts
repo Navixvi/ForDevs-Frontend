0e6eadc5196d521ddfeba2884fb0db2a892785f4
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-10-01 10:13:23 -0300

    feat register working

--- src/lib/auth-util.ts
+++ src/lib/auth-util.ts
@@ -1,8 +1,18 @@
 export const register = async (userData: any): Promise<any> => {
   const response = await fetch("http://localhost:3000/auth/register", {
     method: "POST",
-    body: JSON.stringify(userData)
-  }); // Cambiar por contante env
+    headers: {
+      'Content-Type': 'application/json',  // Envía JSON
+    },
+    body: JSON.stringify(userData),  // Convierte el objeto a JSON
+  });
+
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    console.error('Error details:', errorResponse);  // Muestra los detalles del error
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
   const registeredUser = await response.json();
   return registeredUser;
 };
