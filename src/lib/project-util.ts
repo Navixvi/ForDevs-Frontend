e1a66fb85c87fbaee6b14bd8e581e0c6d40299b7
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-12-05 16:49:00 -0300

    fix fetch project route

--- src/lib/project-util.ts
+++ src/lib/project-util.ts
@@ -1,7 +1,8 @@
 import { Project } from "@/app/home/types";
 
 export const fetchProjects = async (): Promise<Project[]> => {
-  const response = await fetch("http://localhost:3000/projects"); // Cambiar por contante env
+  const response = await fetch("http://localhost:3000/project"); // Cambiar por contante env
   const projects = await response.json();
+
   return projects;
 };
